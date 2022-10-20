b413e29e5ea9ed68bf901ef10f3c4ac2800df9c4
w3Ayananth <tsayananth93@gmail.com>
2022-10-20 11:09:49 +0530

    login required to access some pages

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
 from . models import Post
 
 
 
 # Create your views here.
+@login_required
 def home(request):
     context = {'posts': Post.objects.all()}
     return render(request, 'blog/home.html', context)
 
+@login_required
 def about(request):
     return render(request, 'blog/about.html')
