30e4a0ab1ee8713ceffa447a2ad2bd45b549383e
w3Ayananth <tsayananth93@gmail.com>
2022-10-27 17:35:37 +0530

    password reset

--- project_blogapp/settings.py
+++ project_blogapp/settings.py
@@ -126,3 +126,11 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL ='login'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'ayananth.ts@travancoreanalytics.com'
+EMAIL_HOST_PASSWORD = 'Ayananth@!@#'
